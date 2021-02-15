1872e0a167b580717b08d53f531387644923e5a2
Florian Hennig <florian.hennig@committance.com>
2021-02-15 12:28:36 +0100

    add embedded tileset

---
+++ committance_haken_128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="committance3" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+ <image source="../Logos/committance_haken_128_tile.png" width="128" height="128"/>
+</tileset>
